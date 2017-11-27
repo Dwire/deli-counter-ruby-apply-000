54fc563d971030a8fa74ddef72a86451db90b536
Greg Dwyer <gjdwyer8@gmail.com>
2017-11-27 22:55:23 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -35,6 +35,9 @@
 
 # Write your code here.
 
+katz_deli = []
+other_deli = ["Logan", "Avi", "Spencer"]
+another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
 
 
   def line(arr)
