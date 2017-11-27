fcdafda583101cdaa8042303144ec06ab7d9f258
Greg Dwyer <gjdwyer8@gmail.com>
2017-11-27 21:59:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,22 +1,24 @@
 # Write your code here.
 class DeliCounter
 
- katz_deli = []
+ @@katz_deli = []
 
 # decouple position & name to increase usability --- return value example ["Greg 1.", "Kurt 2.", "Jake 3."]
- def line(arr)
+ def line
    current_line =[]
 
-   if katz_deli.length == 0
+   if @@katz_deli.length == 0
      puts "The line is currently empty."
    else
-     katz_deli.each_with_index {|n, i| current_line << "#{n} #{i + 1}."}
+     @@katz_deli.each_with_index {|n, i| current_line << "#{n} #{i + 1}."}
      puts "The line is currently: #{current_line.join(" ")}"
    end
    current_line
  end
 
  def take_a_number
+   if line.length == 0
+     puts
  end
 
  def now_serving
