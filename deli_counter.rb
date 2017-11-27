cd928b9e5cb17916f554192b3e9337c2dbaea8bf
Greg Dwyer <gjdwyer8@gmail.com>
2017-11-27 22:14:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,28 +1,28 @@
 # Write your code here.
 class DeliCounter
 
- @@katz_deli = []
-
+  @@katz_deli = []
 # decouple position & name to increase usability --- return value example ["Greg 1.", "Kurt 2.", "Jake 3."]
- def line
-   current_line =[]
+  def line
+    current_line =[]
 
-   if @@katz_deli.length == 0
-     puts "The line is currently empty."
-   else
-     @@katz_deli.each_with_index {|n, i| current_line << "#{n} #{i + 1}."}
-     puts "The line is currently: #{current_line.join(" ")}"
-   end
-   current_line
- end
+    if @@katz_deli.length == 0
+      puts "The line is currently empty."
+    else
+      @@katz_deli.each_with_index {|n, i| current_line << "#{n} #{i + 1}."}
+      puts "The line is currently: #{current_line.join(" ")}"
+    end
+    current_line
+  end
 
- def take_a_number(name)
-   if line.length == 0
-     @@katz_deli << name
-     puts "Welcome, #{name}. You are number 1 in line."
-   else
-     puts "Welcome, #{name}. You are number #{@@katz_deli.length + 1} in line"
- end
+  def take_a_number(name)
+    if line.length == 0
+      puts "Welcome, #{name}. You are number 1 in line."
+    else
+      puts "Welcome, #{name}. You are number #{@@katz_deli.length + 1} in line"
+    end
+    @@katz_deli << name
+  end
 
  def now_serving
 
