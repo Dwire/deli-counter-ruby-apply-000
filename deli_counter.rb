78fb353542ed5f10da2d5a3fdde521d862313b72
Greg Dwyer <gjdwyer8@gmail.com>
2017-11-27 22:48:51 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,49 @@
+# # Write your code here.
+# class DeliCounter
+#
+#   def initialize(deli = [])
+#     @katz_deli = deli
+#   end
+#
+#   def line
+#     current_line =[]
+#
+#     if @katz_deli.length == 0
+#       puts "The line is currently empty."
+#     else
+#       @katz_deli.each_with_index {|n, i| current_line << "#{n} #{i + 1}."}
+#       puts "The line is currently: #{current_line.join(" ")}"
+#     end
+#     current_line
+#   end
+#
+#   def take_a_number(name)
+#     if line.length == 0
+#       puts "Welcome, #{name}. You are number 1 in line."
+#     else
+#       puts "Welcome, #{name}. You are number #{@katz_deli.length + 1} in line"
+#     end
+#     @katz_deli << name
+#   end
+#
+#   def now_serving
+#     puts line.length == 0 ? "There is nobody waiting to be served!" : "Currently serving #{@katz_deli[0]}"
+#     @katz_deli.shift
+#   end
+#
+# end
+
 # Write your code here.
-class DeliCounter
 
-  def initialize(deli = [])
-    @katz_deli = deli
-  end
+  katz_deli = []
 
-  def line
+  def line(arr)
     current_line =[]
 
-    if @katz_deli.length == 0
+    if arr.length == 0
       puts "The line is currently empty."
     else
-      @katz_deli.each_with_index {|n, i| current_line << "#{n} #{i + 1}."}
+      arr.each_with_index {|n, i| current_line << "#{n} #{i + 1}."}
       puts "The line is currently: #{current_line.join(" ")}"
     end
     current_line
@@ -21,9 +53,9 @@ class DeliCounter
     if line.length == 0
       puts "Welcome, #{name}. You are number 1 in line."
     else
-      puts "Welcome, #{name}. You are number #{@katz_deli.length + 1} in line"
+      puts "Welcome, #{name}. You are number #{line.length + 1} in line"
     end
-    @katz_deli << name
+    katz_deli << name
   end
 
   def now_serving
