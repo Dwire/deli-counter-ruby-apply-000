e2d42790cd6d9901578ce86bc0f38a98829888dc
Greg Dwyer <gjdwyer8@gmail.com>
2017-11-27 23:03:38 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -66,7 +66,7 @@ another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi"
     katz_deli.shift
   end
 
-
-line(katz_deli)
-line(other_deli)
-take_a_number(other_deli, "Josh")
+# 
+# line(katz_deli)
+# line(other_deli)
+# take_a_number(other_deli, "Josh")
