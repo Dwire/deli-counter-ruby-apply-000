118327c56b2fc6a33bf25c7149c8652d9786389d
Greg Dwyer <gjdwyer8@gmail.com>
2017-11-27 21:48:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ class DeliCounter
      arr.each_with_index {|n, i| current_line << "#{n} #{i + 1}."}
      puts "The line is currently: #{current_line.join(" ")}"
    end
-   current_line
+   p current_line
  end
 
  def take_a_number
